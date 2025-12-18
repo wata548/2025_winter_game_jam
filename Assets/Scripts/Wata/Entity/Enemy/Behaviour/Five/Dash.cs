e64a381dbaa93c752d1d0b9143deeda89d6ebadf
wata548 <watashimokia@gmail.com>
2025-12-19 02:38:19 +0900

    Add vfx asset, five color,

---
+++ Assets/Scripts/Wata/Entity/Enemy/Behaviour/Five/Dash.cs
@@ -0,0 +1,22 @@
+﻿using Entity.Enemy.FSM;
+using UnityEngine;
+
+namespace Entity.Enemy.Behaviour.Five {
+    public class Dash: AnimationBehaviour {
+        [SerializeField] private float _speed;
+
+        protected override void OnMiddle() {
+            var fsm = GetComponent<TargetFSM>();
+            var direction = Mathf.Sign(fsm.Target.GetComponent<Collider>().bounds.center.x
+                             - fsm.Movement.Collider.bounds.center.x);
+
+            fsm.Movement.SetHorizonPower(_speed * direction);
+        }
+
+        protected override void OnAfter() {
+            var fsm = GetComponent<TargetFSM>();
+            fsm.Movement.SetHorizonPower(0);
+            _animator.Play("Idle", 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Wata/Entity/Enemy/Behaviour/Five/Smash.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Entity.Enemy.FSM;
+using UnityEngine;
+
+namespace Entity.Enemy.Behaviour.Five {
+    public class Smash: SpecialAnimationBehaviour {
+        public override float Term { get; protected set; } = 8;
+        [SerializeField] private float _radius = 3;
+        [SerializeField] private ParticleSystem _particle;
+
+        protected override void OnMiddle() {
+            var particle = Instantiate(_particle);
+            var fsm = GetComponent<TargetFSM>();
+            
+            var pos = fsm.Movement.Collider.bounds.center;
+            pos.y -= fsm.Movement.Collider.bounds.size.y / 2;
+            particle.transform.position = pos;
+            particle.transform.localScale = Vector3.one * (_radius * 0.7f);
+
+            var player = Physics.OverlapSphere(pos, _radius).FirstOrDefault(collider => collider.CompareTag("Player"));
+            player?.GetComponent<IEntity>()?.GetDamage(1);
+        }
+    }
+}
